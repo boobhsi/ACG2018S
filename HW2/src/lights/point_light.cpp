220e6dc1a971b8fee18f3e02e9317eabb7d839f6
Eduardo <eduardo.munera@mindtronicai.com>
2018-04-11 19:25:16 +0800

    submit

--- HW2/src/lights/point_light.cpp
+++ HW2/src/lights/point_light.cpp
@@ -1,5 +1,4 @@
 #include <lights/point_light.h>
-#include <cassert>
 
 PointLight::PointLight(vec4 pos, vec3 color, float pow) : Light(pos, color, pow) {
     
@@ -11,8 +10,8 @@ PointLight::~PointLight() {
 
 float PointLight::get_illuminance(vec4 pos) {
     float distance = (pos - position_t).length2();
-    assert(distance > 0.001f);
-    float i = power / distance;
+    //assert(distance > 0.001f);
+    if(distance < 0.00001f) return power;
+    return power / distance;
     //float i = power;
-    return i;
 }
